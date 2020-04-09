332b27b7f0b6c2d28927c7ba3ab47be247ac591c
Ewelai <palevo.ewelai@gmail.com>
2020-04-09 18:44:32 +0300

    fix: eslintignore edited

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import classes from "./Home.module.css";
+import classes from './Home.module.css';
 
-console.log('efef')
+console.log('efef');
 
 const home = () => <div className={classes.container}>Home</div>;
 
