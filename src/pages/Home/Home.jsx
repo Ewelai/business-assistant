b3df6863692259b09a3158e85c4b377a4a1f07f1
Ewelai <palevo.ewelai@gmail.com>
2020-04-09 18:46:39 +0300

    fix: package.json

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import classes from './Home.module.css';
 
-console.log('efef');
-
 const home = () => <div className={classes.container}>Home</div>;
 
 export default home;
